b6003a762c48cf45e291116f1a65b2584ca70e43
Janis Wolf <wolfjanis@gmail.com>
2021-10-13 16:00:14 +0200

    add Welcome Activity

---
+++ app/app/src/main/java/ch/zice/spp/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package ch.zice.spp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
