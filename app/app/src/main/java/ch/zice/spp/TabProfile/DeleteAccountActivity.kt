fcfed38cd7f1e37fccb692aaf3a1657b145a3182
Janis Wolf <wolfjanis@gmail.com>
2021-10-15 12:33:30 +0200

    add send friend request and delete account activity

---
+++ app/app/src/main/java/ch/zice/spp/TabProfile/DeleteAccountActivity.kt
@@ -0,0 +1,12 @@
+package ch.zice.spp.TabProfile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.zice.spp.R
+
+class DeleteAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete_account)
+    }
+}
