cab4689234021481df34bd051614646959f9b6df
Janis Wolf <wolfjanis@gmail.com>
2021-10-15 15:40:36 +0200

    add splashscreen

---
+++ app/app/src/main/java/ch/zice/spp/SplashscreenActivity.kt
@@ -0,0 +1,31 @@
+package ch.zice.spp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        supportActionBar?.hide(); // hide the title bar
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        @Suppress("DEPRECATION")
+       Handler().postDelayed(
+           {
+               startActivity(Intent(this, WelcomeActivity::class.java))
+               finish()
+           }, 1000
+       )
+
+
+    }
+}
