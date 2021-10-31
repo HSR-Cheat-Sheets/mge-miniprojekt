027f1bad121e810b2210fb7aeeecd7429e657d25
Janis Wolf <wolfjanis@gmail.com>
2021-10-31 15:08:35 +0100

    start implementing add party

--- app/app/src/main/java/ch/zice/spp/fragments/PartiesFragment.kt
+++ app/app/src/main/java/ch/zice/spp/fragments/PartiesFragment.kt
@@ -1,5 +1,6 @@
 package ch.zice.spp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,6 +11,8 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
+import ch.zice.spp.EditParty
+import ch.zice.spp.EditProfileActivity
 import ch.zice.spp.R
 import ch.zice.spp.utils.firestore.FirestoreClass
 import com.google.firebase.firestore.FirebaseFirestore
@@ -28,9 +31,17 @@ class PartiesFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parties, container, false)
+
+
+        val view: View = inflater.inflate(R.layout.fragment_parties, container, false)
+
+        view.findViewById<Button>(R.id.add_party_button).setOnClickListener(){
+            activity?.startActivity(Intent(activity, EditParty::class.java).apply{})
+        }
+
+        return view
     }
 
 
